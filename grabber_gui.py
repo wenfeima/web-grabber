52449ae840402b2acdb15b075218567aba0198e8
wenfeima <wenfeima@users.noreply.github.com>
2026-09-05 11:11:59 +0800

    v34 fix3: 修复rowconfigure行号(8->9)，按钮行被设置weight导致页签被挤走

--- grabber_gui.py
+++ grabber_gui.py
@@ -286,7 +286,7 @@ class GrabberApp:
         self.nb = ttk.Notebook(frm)
         nb = self.nb
         nb.grid(row=9, column=0, columnspan=6, sticky='nsew', padx=5, pady=4)
-        frm.rowconfigure(8, weight=1)
+        frm.rowconfigure(9, weight=1)
 
         # --- 页1：任务列表（表格 + 统计栏） ---
         tab_list = ttk.Frame(nb)
